da510598619e296914aca9aaf3590983b590b31d
unknown <apache00013@gmail.com>
2018-10-01 23:08:44 +0700

    fix frontend layer

--- resources/views/frontend/service/load.blade.php
+++ resources/views/frontend/service/load.blade.php
@@ -1,11 +1,11 @@
 <div class="row" id="load" style="position: relative;">
     @foreach ($technologies as $key => $item)
-    <div class="col-md-12" style="background-image: url({{ asset('storage/uploads/' . $item->path . '/picture/' . $item->picture_name) }}); background-size: 100% 100%; background-repeat: no-repeat; min-height: 390px; width: 100%; margin-bottom: 10px;">
+    <div class="col-md-12" style="background-image: url({{ asset('storage/uploads/' . $item->path . '/picture/' . $item->picture_name) }}); background-size: 100% 100%; background-repeat: no-repeat; min-height: 400px; width: 100%; margin-bottom: 10px;">
         @php $layer = 1 @endphp
-        @for ($i = 1; $i <= 3; $i++)
+        @for ($i = 1; $i <= 4; $i++)
             <div class="row">
-            @for ($j = 1; $j <= 6; $j++)
-                <div class="col-md-2 col-xs-2" style="min-height: 130px;">
+            @for ($j = 1; $j <= 12; $j++)
+                <div class="col-md-1 col-xs-1" style="min-height: 98px;">
                     <div class="row" @if (!empty($item->equipment_assignment[$layer]))data-toggle="tooltip" data-html="true" title="<h3>{{ $item->equipment_assignment[$layer]['equipment_name'] }}</h3> {{ $item->equipment_assignment[$layer]['equipment_detail'] }}"@endif>
                         @if (!empty($item->equipment_assignment[$layer]))
                             <h3 class="text-center"><i class="fa fa-info-circle"></i></h3>
